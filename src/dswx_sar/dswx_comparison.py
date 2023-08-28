f241588d370fc3829b2f9f1ae637446461469df9
Jungkyo Jung <jungkyoj@aurora.jpl.nasa.gov>
2023-08-28 15:53:03 -0700

    fix minor

--- src/dswx_sar/dswx_comparison.py
+++ src/dswx_sar/dswx_comparison.py
@@ -44,16 +44,16 @@ def _print_first_value_diff(image_1, image_2, prefix):
     flag_error_found = False
     for i in range(image_1.shape[0]):
         for j in range(image_1.shape[1]):
-            if (abs(image_1[i, j] - image_2[i, j]) <=
+            if (abs(image_1[i, j] - image_2[i, j]) >=
                     COMPARE_DSWX_SAR_PRODUCTS_ERROR_TOLERANCE):
-                continue
-            print(prefix + f'     * input 1 has value'
-                  f' "{image_1[i, j]}" in position'
-                  f' (x: {j}, y: {i})'
-                  f' whereas input 2 has value "{image_2[i, j]}"'
-                  ' in the same position.')
-            flag_error_found = True
-            break
+                flag_error_found = True
+
+                print(prefix + f'     * input 1 has value'
+                    f' "{image_1[i, j]}" in position'
+                    f' (x: {j}, y: {i})'
+                    f' whereas input 2 has value "{image_2[i, j]}"'
+                    ' in the same position.')
+                break
         if flag_error_found:
             break
 
@@ -85,24 +85,26 @@ def _compare_dswx_sar_metadata(metadata_1, metadata_2):
             metadata_error_message += (' Input 2 metadata has extra entries'
                                        ' with keys:'
                                        f' {", ".join(set_2_m_1)}.')
-    else:
-        for k1, v1, in metadata_1.items():
-            if k1 not in metadata_2.keys():
-                flag_same_metadata = False
-                metadata_error_message = (
-                    f'* the metadata key {k1} is present in'
-                    ' but it is not present in input 2')
-                break
-            # Exclude metadata fields that are not required to be the same
-            if k1 in COMPARISON_EXCEPTION_LIST:
-                continue
-            if metadata_2[k1] != v1:
-                flag_same_metadata = False
-                metadata_error_message = (
-                    f'* contents of metadata key {k1} from'
-                    f' input 1 has value "{v1}" whereas the same key in'
-                    f' input 2 metadata has value "{metadata_2[k1]}"')
-                break
+        return metadata_error_message, flag_same_metadata
+
+    for k1, v1, in metadata_1.items():
+        if k1 not in metadata_2.keys():
+            flag_same_metadata = False
+            metadata_error_message = (
+                f'* the metadata key {k1} is present in'
+                ' but it is not present in input 2')
+            break
+        # Exclude metadata fields that are not required to be the same
+        if k1 in COMPARISON_EXCEPTION_LIST:
+            continue
+        if metadata_2[k1] != v1:
+            flag_same_metadata = False
+            metadata_error_message = (
+                f'* contents of metadata key {k1} from'
+                f' input 1 has value "{v1}" whereas the same key in'
+                f' input 2 metadata has value "{metadata_2[k1]}"')
+            break
+
     return metadata_error_message, flag_same_metadata
 
 def compare_dswx_sar_products(file_1, file_2):
@@ -205,4 +207,5 @@ def main():
 
 
 if __name__ == '__main__':
+
     main()

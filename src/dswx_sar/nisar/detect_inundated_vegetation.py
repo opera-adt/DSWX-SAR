b2a398c674ed5bdbb6078eb6cf6e4dec462e3d8b
Jungkyo Jung <jungkyoj@aurora.jpl.nasa.gov>
2025-10-30 00:34:10 -0700

    water_cloud_model

--- src/dswx_sar/nisar/detect_inundated_vegetation.py
+++ src/dswx_sar/nisar/detect_inundated_vegetation.py
@@ -8,8 +8,10 @@ from dswx_sar.common import _filter_SAR, _generate_log
 import numpy as np
 
 from dswx_sar.common import _dswx_sar_util
-from dswx_sar.sentinel1.dswx_runconfig import DSWX_NI_POL_DICT, _get_parser, RunConfig
+from dswx_sar.nisar.dswx_ni_runconfig import DSWX_NI_POL_DICT, _get_parser, RunConfig
 from dswx_sar.common import _detect_inundated_vegetation
+
+from dswx_sar.common._detect_inundated_vegetation import (pow2db, db2pow)
 from dswx_sar.common._pre_processing import pol_ratio
 from dswx_sar.common._masking_with_ancillary import FillMaskLandCover
 
@@ -34,11 +36,8 @@ def run(cfg):
     pol_all_str = '_'.join(pol_list)
 
     inundated_vege_cfg = processing_cfg.inundated_vegetation
-    inundated_vege_ratio_max = inundated_vege_cfg.dual_pol_ratio_max
-    inundated_vege_ratio_min = inundated_vege_cfg.dual_pol_ratio_min
-    inundated_vege_ratio_threshold = \
-        inundated_vege_cfg.dual_pol_ratio_threshold
     inundated_vege_cross_pol_min = inundated_vege_cfg.cross_pol_min
+    inundated_vege_method = inundated_vege_cfg.inundated_vegetation_method
 
     target_file_type = inundated_vege_cfg.target_area_file_type
     target_worldcover_class = inundated_vege_cfg.target_worldcover_class
@@ -48,9 +47,28 @@ def run(cfg):
     filter_options = inundated_vege_cfg.filter
     filter_method = inundated_vege_cfg.filter.method
 
+    if inundated_vege_method == 'dual_pol_ratio':
+        dual_pol_options_cfg = inundated_vege_cfg.dual_pol_ratio_options
+        inundated_vege_ratio_max = dual_pol_options_cfg.dual_pol_ratio_max
+        inundated_vege_ratio_min = dual_pol_options_cfg.dual_pol_ratio_min
+        inundated_vege_ratio_threshold = \
+            dual_pol_options_cfg.dual_pol_ratio_threshold
+
+    elif inundated_vege_method == 'water_cloud':
+        water_cloud_options_cfg = inundated_vege_cfg.water_cloud_model_options
+        ratio_diff_threshold = water_cloud_options_cfg.dual_pol_ratio_difference_threshold
+        height_data_type = water_cloud_options_cfg.height_data_type
+
+    else:
+        err_str = f"Inundated Vegetation Method {inundated_vege_method}' is not supported"
+        raise ValueError(err_str)
+
     interp_glad_path_str = os.path.join(scratch_dir, 'interpolated_glad.tif')
     interp_worldcover_path_str = os.path.join(scratch_dir,
                                               'interpolated_landcover.tif')
+    interp_eth_path_str = os.path.join(
+        scratch_dir,
+        'interpolated_eth.tif')
 
     if target_file_type == 'auto':
         if os.path.exists(interp_glad_path_str):
@@ -103,10 +121,11 @@ def run(cfg):
         err_str = f'{rtc_dual_path} is not found.'
         raise FileExistsError(err_str)
 
-    if (inundated_vege_ratio_min > inundated_vege_ratio_threshold) or \
-       (inundated_vege_ratio_max < inundated_vege_ratio_threshold):
-        err_str = f'{inundated_vege_ratio_threshold} is not valid.'
-        raise ValueError(err_str)
+    if inundated_vege_method == 'dual_pol_ratio':
+        if (inundated_vege_ratio_min > inundated_vege_ratio_threshold) or \
+        (inundated_vege_ratio_max < inundated_vege_ratio_threshold):
+            err_str = f'{inundated_vege_ratio_threshold} is not valid.'
+            raise ValueError(err_str)
 
     im_meta = _dswx_sar_util.get_meta_from_tif(rtc_dual_path)
 
@@ -123,9 +142,12 @@ def run(cfg):
             rtc_dual_path,
             block_param)
 
+        co_pol_image = np.squeeze(rtc_dual[copol_ind, :, :])
+        cross_pol_image = np.squeeze(rtc_dual[crosspol_ind, :, :])
+
         rtc_ratio = pol_ratio(
-            np.squeeze(rtc_dual[copol_ind, :, :]),
-            np.squeeze(rtc_dual[crosspol_ind, :, :]))
+            co_pol_image,
+            cross_pol_image)
 
         if filter_method == 'lee':
             filtering_method = _filter_SAR.lee_enhanced_filter
@@ -145,15 +167,24 @@ def run(cfg):
 
         filt_ratio = filtering_method(
                         rtc_ratio, **filter_option)
-        filt_ratio_db = 10 * np.log10(filt_ratio +
-                                      _dswx_sar_util.Constants.negligible_value)
-        cross_db = 10 * np.log10(
-            np.squeeze(rtc_dual[crosspol_ind, :, :]) +
-            _dswx_sar_util.Constants.negligible_value)
+        filt_ratio_db = _detect_inundated_vegetation.pow2db(
+            filt_ratio + _dswx_sar_util.Constants.negligible_value)
+
+        filt_cross = filtering_method(
+                np.squeeze(cross_pol_image), **filter_option)
+        filtered_cross_db = _detect_inundated_vegetation.pow2db(
+            filt_cross + _dswx_sar_util.Constants.negligible_value)
+
+        if inundated_vege_method == 'water_cloud':
+            filt_co = filtering_method(
+                np.squeeze(co_pol_image), **filter_option)
+            filtered_co_db = _detect_inundated_vegetation.pow2db(
+                filt_co + _dswx_sar_util.Constants.negligible_value)
 
         output_data = np.zeros(filt_ratio.shape, dtype='uint8')
 
-        target_cross_pol = cross_db > inundated_vege_cross_pol_min
+        target_cross_pol = filtered_cross_db > inundated_vege_cross_pol_min
+
         if target_file_type == 'WorldCover':
             target_inundated_vege_class = mask_obj.get_mask(
                 mask_label=target_worldcover_class,
@@ -178,14 +209,158 @@ def run(cfg):
                                                    dtype='int8')
             target_inundated_vege_class[
                 glad_no_data & target_replace_class] = 2
-                # target_replace_class[glad_no_data]
 
         no_data = np.isnan(filt_ratio)
         target_inundated_vege_class[no_data] = 0
 
-        all_inundated_cand = \
-            (filt_ratio_db > inundated_vege_ratio_threshold) & \
-            target_cross_pol
+        if 'dual_pol_ratio' in inundated_vege_method:
+            all_inundated_cand = \
+                (filt_ratio_db > inundated_vege_ratio_threshold) & \
+                target_cross_pol
+
+        elif 'water_cloud' in inundated_vege_method:
+            # Methods for forest and short vegetation are different.
+            # For the forest, we apply the water-cloud model for the dry forest,
+            # and simulate the backscattering for forest height.
+            # We compute the expect dual-pol ratio and compare it with observed ratio.
+            # If forest show higher than the expected ratio value, then those areas are
+            # defined as inundated forest
+
+            # glad_height = mask_obj.get_mask(block_param=block_param)
+            glad_height = _dswx_sar_util.get_raster_block(
+                interp_glad_path_str,
+                block_param)
+            # Inundated Forest
+            if height_data_type == 'GLAD':
+                vege_param_image = np.zeros(glad_height.shape)
+                wet_forest_mask = np.logical_and(glad_height >= 125,
+                                                    glad_height <= 148)
+                dry_forest_mask = np.logical_and(glad_height >= 25,
+                                                    glad_height <= 48)
+
+                # class 125 represents the 3 m tree
+                vege_param_image[wet_forest_mask] = \
+                    glad_height[wet_forest_mask] \
+                    - 125 + 3
+                vege_param_image[dry_forest_mask] = \
+                    glad_height[dry_forest_mask] - 22 + 3
+
+            elif height_data_type == 'ETH_Global_Height':
+                wet_forest_mask = np.logical_and(
+                    glad_height >= 100,
+                    glad_height <= 148)
+                dry_forest_mask = np.logical_and(
+                    glad_height >= 0,
+                    glad_height <= 48)
+                vege_param_image = _dswx_sar_util.get_raster_block(
+                    interp_eth_path_str,
+                    block_param)
+            vege_param_image = vege_param_image.astype('float')
+            (all_inundated_cand_forest,
+            simul_forest_co_pol,
+            simul_forest_x_pol,
+            simul_forest_ratio) = _detect_inundated_vegetation.detect_iv_with_water_cloud(
+                co_pol_image=co_pol_image,
+                cross_pol_image=cross_pol_image,
+                vegetation_param_image=vege_param_image,
+                dry_forest_mask=dry_forest_mask,
+                wet_forest_mask=wet_forest_mask,
+                initial_guess_co_pol=[0.141, 0.5, 0.2, 0.05, 1.0],
+                initial_guess_cross_pol=[0.01, 0.1, 0.2, 0.05, 1.0],
+                lower_bounds_co=[db2pow(-15), db2pow(-10), 0, 0, 0],
+                lower_bounds_cross=[db2pow(-20), db2pow(-15), 0, 0, 0],
+                upper_bounds_co=[db2pow(-2), db2pow(0), 2, 2, 15],
+                upper_bounds_cross=[db2pow(-4), db2pow(0), 2, 2, 15],
+                ratio_threshold=ratio_diff_threshold
+                )
+
+
+            if height_data_type == 'GLAD':
+                # Inundated short vegetation
+                # Since glad product has underestimated and not-precise height for short vegetation,
+                # we cannot simulate the water cloud mode. Thus, we use the dual thresholds for co/cross-ratio.
+                glad_height = _dswx_sar_util.get_raster_block(
+                    interp_glad_path_str,
+                    block_param)
+                short_vege_param_image = np.zeros(glad_height.shape)
+                wet_short_vege_mask = np.logical_and(
+                    glad_height >= 100,
+                    glad_height <= 124
+                    )
+                dry_short_vege_mask = np.logical_and(
+                    glad_height >= 0,
+                    glad_height <= 24)
+                occasional_open_water = np.logical_and(
+                    glad_height >= 200,
+                    glad_height <= 207)
+                target_short_vege_area = wet_short_vege_mask | occasional_open_water
+                short_vege_param_image[wet_short_vege_mask] = \
+                    glad_height[wet_short_vege_mask] \
+
+                short_vege_param_image[dry_short_vege_mask] = \
+                    glad_height[dry_short_vege_mask]
+
+
+                short_vege_co_threshold = -16
+                short_vege_cross_threshold = -18
+                short_vege_ratio_threshold  = 10.6
+
+                dark_iv = np.logical_and(filtered_co_db < short_vege_co_threshold,
+                    filtered_cross_db < short_vege_cross_threshold)
+                bright_iv = filt_ratio_db > short_vege_ratio_threshold
+
+                rtc_short_iv = dark_iv | bright_iv
+                all_inundated_cand_short = rtc_short_iv & target_short_vege_area
+
+
+                if processing_cfg.debug_mode:
+                    out_rasters = [
+                        os.path.join(scratch_dir, 'dark_iv.tif'),
+                        os.path.join(scratch_dir, 'bright_iv.tif'),
+                        os.path.join(scratch_dir, 'all_inundated_cand_short.tif')
+                    ]
+
+                    data_list = [dark_iv, bright_iv, all_inundated_cand_short]
+
+                    for out_raster, data in zip(out_rasters, data_list):
+                        print(out_raster)
+                        _dswx_sar_util.write_raster_block(
+                            out_raster=out_raster,
+                            data=data,
+                            block_param=block_param,
+                            geotransform=im_meta['geotransform'],
+                            projection=im_meta['projection'],
+                            datatype='float32',
+                            cog_flag=True,
+                            scratch_dir=scratch_dir)
+
+                all_inundated_cand = all_inundated_cand_short | all_inundated_cand_forest
+
+            elif height_data_type == 'ETH_Global_Height':
+                all_inundated_cand = all_inundated_cand_forest
+
+
+            if processing_cfg.debug_mode:
+                out_rasters = [
+                    os.path.join(scratch_dir, 'simul_forest_co_pol.tif'),
+                    os.path.join(scratch_dir, 'simul_forest_cross_pol.tif'),
+                    os.path.join(scratch_dir, 'simul_forest_ratio_db.tif')
+                ]
+
+                data_list = [simul_forest_co_pol, simul_forest_x_pol, simul_forest_ratio]
+
+                for out_raster, data in zip(out_rasters, data_list):
+                    _dswx_sar_util.write_raster_block(
+                        out_raster=out_raster,
+                        data=data,
+                        block_param=block_param,
+                        geotransform=im_meta['geotransform'],
+                        projection=im_meta['projection'],
+                        datatype='float32',
+                        cog_flag=True,
+                        scratch_dir=scratch_dir
+                    )
+
         inundated_vegetation = all_inundated_cand & \
             (target_inundated_vege_class > 0)
         output_data[inundated_vegetation] = 2
@@ -232,8 +407,6 @@ def run(cfg):
                 cog_flag=True,
                 scratch_dir=scratch_dir)
 
-    _dswx_sar_util._save_as_cog(inundated_vege_path, scratch_dir)
-
     t_time_end = time.time()
 
     logger.info(
@@ -257,7 +430,7 @@ def main():
         return
 
     if flag_first_file_is_text:
-        cfg = RunConfig.load_from_yaml(args.input_yaml[0], 'dswx_s1', args)
+        cfg = RunConfig.load_from_yaml(args.input_yaml[0], 'dswx_ni', args)
 
     processing_cfg = cfg.groups.processing
     pol_mode = processing_cfg.polarization_mode
